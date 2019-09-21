a7c3f00de03a57be0b8cb070398620fafcccba20
yourname <xiaozui@git.com>
2019-09-21 15:40:59 +0800

    超级自由行后台打包能力接大工厂

--- src/main/java/com/tomcat/FindGirlServlet.java
+++ src/main/java/com/tomcat/FindGirlServlet.java
@@ -19,7 +19,7 @@ public class FindGirlServlet extends MyServlet{
 	public void doGet(MyRequest myRequest, MyResponse myResponse) {
 		// TODO Auto-generated method stub
 		try {
-			myResponse.write("../com/target/test.html");
+			myResponse.write("../resources/test.html");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
